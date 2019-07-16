ff609e3cd65ac4c2d400847d44aa56131802a747
Ken Faubel <ken@faubel.org>
2019-07-15 21:03:34 -0400

    Added xss protection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express'),
-  app = express(),
-  cors = require('cors'),
-  port = process.env.PORT || 80;
-  bodyParser = require('body-parser');
-  
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const xss = require('xss-clean');
+
+const port = process.env.PORT || 80;
+
+const app = express(); 
+app.use(express.json({ limit: '10kb' })); // Body limit is 10 
 app.use(cors());
+app.use(xss());
 app.listen(port);
 
 const basicAuth = require('express-basic-auth')
@@ -12,6 +16,15 @@ const basicAuth = require('express-basic-auth')
 // app.use(basicAuth({
 //    users: { 'ken': 'olivia' }
 // }))
+
+// Rate Limiting
+// const limit = rateLimit({
+//   max: 100,// max requests
+//   windowMs: 60 * 60 * 1000, // 1 Hour of 'ban' / lockout
+//   message: 'Too many requests' // message to send
+// });
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
